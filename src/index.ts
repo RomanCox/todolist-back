e50a8a143860b21531646f38ec5de033df1f3719
Roman_cox <rom.cox@gmail.com>
2023-10-09 16:47:35 +0300

    pre-commit for deploy to vercel

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,25 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: 'localhost',
-    optionsSuccessStatus: 200
-}
+// const corsOptions = {
+//     origin: 'localhost',
+//     optionsSuccessStatus: 200
+// }
 
 const parserMiddleware = bodyParser();
 app.use(parserMiddleware)
-    .use(cors(corsOptions))
-    .use('/todolists', todoListsRouter)
-    .use('/todolists/:todoListId/tasks', tasksRouter)
-
-app.use((req: Request, res: Response) => {
-    res.send(404)
-})
+    //.use(cors(corsOptions))
+    .use(cors())
+    // .use('/todolists', todoListsRouter)
+    // .use('/todolists/:todoListId/tasks', tasksRouter)
 
 app.get('/', (req, res) => {
     res.send('hello! this is todo`s back-end!');
 });
+
+app.use((req: Request, res: Response) => {
+    res.send(404)
+})
 
 app.listen(port, () => {
     console.log(`TodoLists app listening on port ${port}`)
